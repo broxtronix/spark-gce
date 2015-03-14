180176fd1f2fa42c2e219ccc6e6c143354224c61
Michael Broxton <broxton@gmail.com>
2015-03-13 18:10:03 -0700

    Beginning of thorough overhaul of spark_gce.py.  The goal is to conform more closely with spark_ec2.py and to support more options relevant to GCE infrastructure.

--- spark_gce.py
+++ spark_gce.py
@@ -25,138 +25,196 @@ import getpass
 import threading
 import json
 
-###
-# Make sure gcutil is installed and authenticated 
-# Usage: spark_gce.py <project> <no-slaves> <slave-type> <master-type> <identity-file> <zone> <cluster-name>
-# Usage: spark_gce.py <project> <cluster-name> destroy
-###
-
-identity_file = ""
-slave_no = ""
-slave_type = ""
-master_type = ""
-zone = ""
-cluster_name = ""
-username = ""
-project = ""
-
-
-def read_args():
-
-	global identity_file	
-	global slave_no	
-	global slave_type	
-	global master_type
-	global zone
-	global cluster_name
-	global username
-	global project
-
-	if len(sys.argv) == 8:
-		project = sys.argv[1]
-		slave_no = int(sys.argv[2])
-		slave_type = sys.argv[3]
-		master_type = sys.argv[4]
-		identity_file = sys.argv[5]
-		zone = sys.argv[6]
-		cluster_name = sys.argv[7]
-		username = getpass.getuser()
-
-	elif len(sys.argv) == 4 and sys.argv[3].lower() == "destroy":
-
-		print 'Destroying cluster ' + sys.argv[2]
-
-		project = sys.argv[1]
-		cluster_name = sys.argv[2]
-		try:
-
-			command = 'gcloud compute --project ' + project + ' instances list --format json'
-			output = subprocess.check_output(command, shell=True)
-			data = json.loads(output)
-			master_nodes=[]
-			slave_nodes=[]
-
-			for instance in data:
-
-				try:
-					host_name = instance['name']		
-					host_ip = instance['networkInterfaces'][0]['accessConfigs'][0]['natIP']
-					if host_name == cluster_name + '-master':
-						command = 'gcloud compute instances delete ' + host_name + ' --project ' + project
-						command = shlex.split(command)		
-						subprocess.call(command)
-					elif cluster_name + '-slave' in host_name:
-						command = 'gcloud compute instances delete ' + host_name + ' --project ' + project
-						command = shlex.split(command)		
-						subprocess.call(command)
-
-				except:
-					pass				
-
-		except:
-			print "Failed to Delete instances"
-			sys.exit(1)
-
-		sys.exit(0)
-
-	else:
-		print '# Usage: spark_gce.py <project> <no-slaves> <slave-type> <master-type> <identity-file> <zone> <cluster-name>'
-		print '# Usage: spark_gce.py <project> <cluster-name> destroy'
-		sys.exit(0)
-
-		
-
-def setup_network():
-
-	print '[ Setting up Network & Firewall Entries ]'	
-
+# ----------------------------------------------------------------------------------------
+# run() can be used to execute multiple command line processes simultaneously.
+# This is particularly useful for long-running, network-bound processes like
+# starting and stopping nodes with the 'gcloud' command.
+#
+def subprocess_cmd(cmd, result_queue):
+	import subprocess
 	try:
-		command = 'gcloud compute --project=' + project + ' networks create "' + cluster_name + '-network" --range "10.240.0.0/16"'
+		print "  [ CMD ] ", cmd
+		subprocess.check_output(cmd, shell=True)
+		result_queue.put(0)
+	except subprocess.CalledProcessError:
+		return result_queue.put(1)
 
-		command = shlex.split(command)		
-		subprocess.call(command)
+def run(cmds, parallelize = False, stop_on_error = True):
+	import multiprocessing
+	manager = multiprocessing.Manager()
+	result_queue = manager.Queue()
 
-		#Uncomment the above and comment the below section if you don't want to open all ports for public.
-		command = 'gcloud compute firewall-rules delete internal --project '+ project
-		command = 'gcloud compute firewall-rules create internal --network ' + cluster_name + '-network --allow tcp udp icmp --project '+ project
-		command = shlex.split(command)		
-		subprocess.call(command)
-		
-	except OSError:
-		print "Failed to setup Network & Firewall. Exiting.."
+	# Convert non-list commands to a list with just one element
+	if not isinstance(cmds, list):
+		cmds = [ cmds ]
+
+	
+	if not parallelize:
+
+		# Run commands serially
+		for cmd in cmds:
+			subprocess_cmd(cmd, result_queue)
+
+	else: 
+
+		# Start alignment compute processes.
+		jobs = []
+		for cmd in cmds:
+			p = multiprocessing.Process(target=subprocess_cmd, args=(cmd, result_queue))
+			jobs.append(p)
+			p.start()
+
+		# Wait for all the sub-processes to finish processing their queries
+		for p in jobs:
+			p.join()
+
+	# Parse the results.
+	return_vals = []
+	while not result_queue.empty():
+		return_vals.append(result_queue.get())
+			
+	if sum(return_vals) > 0 and stop_on_error:
+		print "\nSubprocess execution failed.  %d out of %d commands returned a non-zero exit status." % (len(return_vals), sum(return_vals))
 		sys.exit(1)
 
-			
-def launch_master():
+# -------------------------------------------------------------------------------------
 
-	print '[ Launching Master ]'
-	command = 'gcloud compute --project "' + project + '" instances create "' + cluster_name + '-master" --zone "' + zone + '" --machine-type "' + master_type + '" --network "' + cluster_name + '-network" --maintenance-policy "MIGRATE" --scopes "https://www.googleapis.com/auth/devstorage.read_only" --image "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/centos-6-v20141218" --boot-disk-type "pd-standard" --boot-disk-device-name "' + cluster_name + '-md"'
+def get_command_prefix(cluster_name, opts):
+	command_prefix = 'gcloud compute'
+	if opts.project:
+		command_prefix += ' --project ' + opts.project
+	return command_prefix
 
-	command = shlex.split(command)		
-	subprocess.call(command)
+def fetch_instance_data(cluster_name, opts):
+	command_prefix = get_command_prefix(cluster_name, opts)
+	command = command_prefix + ' instances list --format json'
+	try: 
+		output = subprocess.check_output(command, shell=True)
+	except subprocess.CalledProcessError:
+		print "An error occured listing instance attributes.  Are you certain that the cluster \'%s\' exists?" % (cluster_name)
+		sys.exit(1)
+		
+	return json.loads(output)
 
 
-def launch_slaves():
+def delete_network(cluster_name, opts):
+	print '[ Deleting Network & Firewall Entries ]'
+	command_prefix = get_command_prefix(cluster_name, opts)
+	cmds = []
+
+	# Uncomment the above and comment the below section if you don't want to open all ports for public.
+	# cmds.append( command_prefix + ' compute firewall-rules delete ' + cluster_name + '-internal --quiet' )
+	cmds.append(  command_prefix + ' firewall-rules delete ' + cluster_name + '-internal --quiet' )
+	cmds.append( command_prefix + ' networks delete "' + cluster_name + '-network" --quiet' )
+	run(cmds, stop_on_error = False)
+
+def setup_network(cluster_name, opts):
+
+	print '[ Setting up Network & Firewall Entries ]'
+	command_prefix = get_command_prefix(cluster_name, opts)
+	cmds = []
+
+	cmds.append( command_prefix + ' networks create "' + cluster_name + '-network" --range "10.240.0.0/16"' )
 	
-	print '[ Launching Slaves ]'
-
-	for s_id in range(1,slave_no+1):
+	# Uncomment the above and comment the below section if you don't want to open all ports for public.
+	# cmds.append( command_prefix + ' compute firewall-rules delete ' + cluster_name + '-internal' )
+	cmds.append( command_prefix + ' firewall-rules create ' + cluster_name + '-internal --network ' + cluster_name + '-network --allow tcp udp icmp' )
+	run(cmds, stop_on_error = False)
 		
-		command = 'gcloud compute --project "' + project + '" instances create "' + cluster_name + '-slave' + str(s_id) + '" --zone "' + zone + '" --machine-type "' + slave_type + '" --network "' + cluster_name + '-network" --maintenance-policy "MIGRATE" --scopes "https://www.googleapis.com/auth/devstorage.read_only" --image "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/centos-6-v20141218" --boot-disk-type "pd-standard" --boot-disk-device-name "' + cluster_name + '-s' + str(s_id) + 'd"'
+def launch_cluster(cluster_name, opts):
+	"""
+	Create a new cluster. 
+	"""
+	print '[ Launching cluster: %s ]' % cluster_name
+	command_prefix = get_command_prefix(cluster_name, opts)
 
-		command = shlex.split(command)		
-		subprocess.call(command)
+	if opts.zone:
+		zone_str = ' --zone ' + opts.zone
+	else:
+		zone_str = ''
+
+	# Set up the network
+	setup_network(cluster_name, opts)
+
+	# Start master node
+	cmds = []
+	cmds.append( command_prefix + ' instances create "' + cluster_name + '-master" --machine-type "' + opts.master_instance_type + '" --network "' + cluster_name + '-network" --maintenance-policy "MIGRATE" --scopes "https://www.googleapis.com/auth/devstorage.read_only" --image "https://www.googleapis.com/compute/v1/projects/gce-nvme/global/images/nvme-backports-debian-7-wheezy-v20141108" --boot-disk-type "' + opts.boot_disk_type + '" --boot-disk-size ' + opts.boot_disk_size + ' --boot-disk-device-name "' + cluster_name + '-md" --local-ssd interface=nvme' + zone_str )
+
+	# Start slave nodes
+	for i in xrange(opts.slaves):
+		cmds.append( command_prefix + '" instances create "' + cluster_name + '-slave' + str(i) + ' --machine-type "' + opts.instance_type + '" --network "' + cluster_name + '-network" --no-address --maintenance-policy "MIGRATE" --scopes "https://www.googleapis.com/auth/devstorage.read_only" --image "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/nvme-backports-debian-7-wheezy-v20141108" --boot-disk-type "' + opts.boot_disk_type + '" --boot-disk-size ' + opts.boot_disk_size + ' --boot-disk-device-name "' + cluster_name + '-s' + str(i) + 'd" --local-ssd interface=nvme' + zone_str )
 		
-def launch_cluster():
+	for cmd in cmds:
+		print cmd
+	#run(cmds, parallelize = True)
+
+
+def destroy_cluster(cluster_name, opts):
+	"""
+	Delete a cluster permenantly.  All state will be lost.
+	"""
+	print '[ Destroying cluster: %s ]'  % (cluster_name)
+	command_prefix = get_command_prefix(cluster_name, opts)
+
+	cmds = []
+	for instance in fetch_instance_data(cluster_name, opts):
+		host_name = instance['name']
+		zone = instance['zone']
+		if host_name == cluster_name + '-master':
+			cmds.append( command_prefix + ' instances delete ' + host_name + ' --zone ' + zone )
+		elif cluster_name + '-slave' in host_name:
+			cmds.append( command_prefix + ' instances delete ' + host_name + ' --zone ' + zone )
+
+	proceed = raw_input('Cluster %s with %d nodes will be deleted permenantly.  Proceed? (y/N) : ' % (cluster_name, len(cmds)))
+	if proceed == 'y' or proceed == 'Y':
+		run(cmds, parallelize = True)
+
+		# Delete the network
+		delete_network(cluster_name, opts)
+	else:
+		print "Exiting without deleting cluster %s." % (cluster_name)
+		sys.exit(0)
+
+		
+def stop_cluster(cluster_name, opts):
+	"""
+	Stop a running cluster. The cluster can later be restarted with the 'start'
+	command. All the data on the root drives of these instances will
+	persist across reboots, but any data on attached drives will be lost.
+	"""
+	print '[ Stopping cluster: %s ]' % cluster_name
+	command_prefix = get_command_prefix(cluster_name, opts)
+
+	cmds = []
+	for instance in fetch_instance_data(cluster_name, opts):
+		host_name = instance['name']
+		zone = instance['zone']
+		if host_name == cluster_name + '-master':
+			cmds.append( command_prefix + ' instances stop ' + host_name + ' --zone ' + zone )
+		elif cluster_name + '-slave' in host_name:
+			coms.append( command_prefix + ' instances stop ' + host_name + ' --zone ' + zone )
+
+	run(cmds, parallelize = True)
+
 	
-	print '[ Creating the Cluster ]'
+def start_cluster(cluster_name, opts):
+	"""
+	Start a cluster that is in the stopped state.
+	"""
+	print '[ Starting cluster: %s ]' % (cluster_name)
 
-	setup_network()	
 
-	launch_master()
+	cmds = []
+	for instance in fetch_instance_data(cluster_name, opts):
+		host_name = instance['name']
+		zone = instance['zone']
+		if host_name == cluster_name + '-master':
+			cmds.append( command_prefix + ' instances start ' + host_name + ' --zone ' + zone )
+		elif cluster_name + '-slave' in host_name:
+			cmds.append( command_prefix + ' instances start ' + host_name + ' --zone ' + zone )
 
-	launch_slaves()
-		
+	run(cmds, parallelize = True)
+
 def check_gcloud():
 	
 	myexec = "gcloud"
@@ -461,48 +519,115 @@ def setup_shark(master_nodes,slave_nodes):
 def show_banner():
 
 	os.system("wget -qO- https://s3.amazonaws.com/sigmoidanalytics-builds/spark/0.9.1/gce/configs/banner")
+
+def parse_args():
 	
+	import os
+	homedir = os.environ['HOME']
+        
+	from optparse import OptionParser
+	parser = OptionParser(
+		usage="spark_gce [options] <action> <cluster_name>"
+		+ "\n\n<action> can be: launch, destroy, login, stop, start",
+		add_help_option=False)
+	parser.add_option(
+		"-h", "--help", action="help",
+		help="Show this help message and exit")
+	parser.add_option(
+		"-s", "--slaves", type="int", default=1,
+		help="Number of slaves to launch (default: %default)")
+	parser.add_option(
+		"-k", "--key-pair",
+		help="Key pair to use on instances")
+	parser.add_option(
+		"-i", "--identity-file", default = os.path.join(homedir, ".ssh", "google_compute_engine"),
+		help="SSH private key file to use for logging into instances")
+	parser.add_option(
+		"-t", "--instance-type", default="n1-highmem-16",
+		help="Type of instance to launch (default: n1-highmen-16).")
+	parser.add_option(
+		"-m", "--master-instance-type", default="n1-highmem-16",
+		help="Master instance type (leave empty for same as instance-type)")
+	parser.add_option(
+		"--boot-disk-type", default="pd-standard",
+		help="Boot disk type.  Run \'gcloud compute disk-types list\' to see your options.")
+	parser.add_option(
+		"--boot-disk-size", default="200GB",
+		help="The size of the boot disk.  Run \'gcloud compute disk-types list\' to see your options.")
+	parser.add_option(
+		"-p", "--project",
+		help="GCE project to target when launching instances ( you can omit this argument if you set a default with \'gcloud config set project [project-name]\'")
+	parser.add_option(
+		"-z", "--zone", default="us-central1-f",
+		help="GCE zone to target when launching instances ( you can omit this argument if you set a default with \'gcloud config set compute/zone [zone-name]\'")
+	
+	(opts, args) = parser.parse_args()
+	if len(args) != 2:
+		parser.print_help()
+		sys.exit(1)
+
+	(action, cluster_name) = args
+	return (opts, action, cluster_name)
+
+
 def real_main():
 
-	show_banner()
+	print "Spark Google Compute Engine v0.2"
+	print ""
 	print "[ Script Started ]"	
-	#Read the arguments
-	read_args()
-	#Make sure gcloud is accessible.
+
+	# Read the arguments
+	(opts, action, cluster_name) = parse_args()
+
+	# Make sure gcloud is accessible.
 	check_gcloud()
 
-	#Launch the cluster
-	launch_cluster()
+	# Launch the cluster
+	if action == "launch":
+		launch_cluster(cluster_name, opts)
 
-	#Wait some time for machines to bootup
-	print '[ Waiting 120 Seconds for Machines to start up ]'
-	time.sleep(120)
+	elif action == "start":
+		start_cluster(cluster_name, opts)
 
-	#Get Master/Slave IP Addresses
-	(master_nodes, slave_nodes) = get_cluster_ips()
+	elif action == "stop":
+		stop_cluster(cluster_name, opts)
 
-	#Install Java and build-essential
-	install_java(master_nodes,slave_nodes)
+	elif action == "destroy":
+		destroy_cluster(cluster_name, opts)
 
-	#Generate SSH keys and deploy
-	deploy_keys(master_nodes,slave_nodes)
+	elif action == "login":
+		login_cluster(cluster_name, opts)
 
-	#Attach a new empty drive and format it
-	attach_drive(master_nodes,slave_nodes)
+	else:
+		print >> stderr, "Invalid action: %s" % action
+		sys.exit(1)
 
-	#Set up Spark/Shark/Hadoop
-	setup_spark(master_nodes,slave_nodes)
+	# #Wait some time for machines to bootup
+	# print '[ Waiting 120 Seconds for Machines to start up ]'
+	# time.sleep(120)
+
+	# #Get Master/Slave IP Addresses
+	# (master_nodes, slave_nodes) = get_cluster_ips()
+
+	# #Install Java and build-essential
+	# install_java(master_nodes,slave_nodes)
+
+	# #Generate SSH keys and deploy
+	# deploy_keys(master_nodes,slave_nodes)
+
+	# #Attach a new empty drive and format it
+	# attach_drive(master_nodes,slave_nodes)
+
+	# #Set up Spark/Shark/Hadoop
+	# setup_spark(master_nodes,slave_nodes)
 
 	
-
-
 def main():
-  try:
-    real_main()
-  except Exception as e:
-    print >> stderr, "\nError:\n", e
-    
+	try:
+		real_main()
+	except Exception as e:
+		print >> stderr, "\nError:\n", e
+		sys.exit(1)
 
 if __name__ == "__main__":
-  
-  main()
+	main()
